cdc18315f87b588332a933c7de3f4a21c08b239e
Nelson Zeng <nelson.zeng25@gmail.com>
2020-01-31 16:25:21 -0500

    Added capitals.csv to get random geo locations

--- Q3-Cache/main.py
+++ Q3-Cache/main.py
@@ -1,10 +1,12 @@
 from cache import *
 from math import sin, cos, sqrt, radians, atan2
 from operator import itemgetter
+import csv
+from random import randint
 
 # https://www.movable-type.co.uk/scripts/latlong.html
 def distance(location1, location2):
-    earthRadius = 6378000
+    earthRadius = 6378.0
     lat1 = radians(location1[0])
     lon1 = radians(location1[1])
     lat2 = radians(location2[0])
@@ -18,18 +20,27 @@ def distance(location1, location2):
     return earthRadius * c
 
 
-def createCaches(number, size, list, location):
+def createCaches(number, size, location):
     cacheDict = {}
+    with open('Q3-Cache/capitals.csv','rt') as file:
+        reader = csv.reader(file)
+        rows = [r for r in reader]
+
     for i in range(number):
-        cache = GeoDistLRUCache(5, (1,1))
+        rowIndex = randint(1,len(rows))
+        cacheLocation = (float(rows[rowIndex][2]), float(rows[rowIndex][3]))
+
+        cache = GeoDistLRUCache(size, cacheLocation)
         cacheDict[cache] = distance(location, cache.location)
 
-    sortedCaches = sorted(cacheDict, key=itemgetter(1))
-    return list(sortedCaches.keys())
+    sortedCaches = sorted(cacheDict, key=cacheDict.get)
+    return sortedCaches
 
 
 if __name__ == '__main__':
-    cacheList = []
-    currentLocation = (0,0)
+    currentLocation = (45.508888, -73.561668)       # Montreal Longitude and Latitude
+    cacheList = createCaches(5, 5, currentLocation)
 
+    for i in cacheList:
+        print(i.location, distance(currentLocation, i.location))
 
